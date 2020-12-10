2715381cbbc4985a965c0ac1998c5ff83dd5f17e
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2020-12-10 14:27:37 +0100

    minimum viable product

--- src/lumipallo/hello.py
+++
@@ -1,2 +0,0 @@
-def world():
-    return 'world'
--- tests/test_lumipallo.py
+++ tests/test_lumipallo.py
@@ -1,4 +1,23 @@
-import lumipallo.hello
+from pathlib import Path
 
-def test_first():
-    assert lumipallo.hello.world() == 'world'
+import pytest
+
+from lumipallo import lumipallo as lp
+
+TESTDATA = Path(__file__).with_name('testdata')
+
+
+@pytest.fixture
+def snow_01_loaded():
+    snow = lp.Snow('deu')
+    snow.load(*list(TESTDATA.glob('01_*.txt')))
+    return snow
+
+
+def test_frequency(snow_01_loaded):
+    assert snow_01_loaded.most_common_words(3) == [('tom', 10), ('bruder', 8), ('heisst', 6)]
+
+
+def test_wordindex(snow_01_loaded):
+    assert len(snow_01_loaded.words) == 15
+    assert snow_01_loaded.words['wie'] == {4, 16}
